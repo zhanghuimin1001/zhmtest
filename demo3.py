2a69639abd94eb2f1d3f7f2d4b676c1f76d6cacc
zhanghm <1545696299@qq.com>
2020-06-08 17:41:50 +0800

    测试

--- demo3.py
+++ demo3.py
@@ -2,15 +2,20 @@ class person():
     name = "王一波"
     sex = "男"
     age =  "22"
-    def see(self):
-        print("看电影了")
+
+    def see(self):    
+        print("{}看电影了".format(self.name))
+
     def hear(self,music):
-        print("听{}".format(music))
-        return "听音乐了"
+        print("{}听{}".format(self.name,music))
+        return "music"
+#self代表类本身，
+
 s = person()
 # print(s.age)
 # print(s.sex)
 # print(s.name)
-
+a = s.see()
 p = s.hear("音乐")
 print(p)
+print(a)
